0b56ebb9e6322893cd7065633eca96aad331def8
jblaha <jakub.blaha@incolor.cz>
2019-12-03 20:12:50 +0100

    windows support

--- spigot-1.13.2/start.bat
+++ spigot-1.13.2/start.bat
@@ -1,3 +1,3 @@
 @echo off
-java -Xms512M -Xmx1G -XX:+UseConcMarkSweepGC -classpath sc-mqtt.jar:spigot-1.13.2.jar org.bukkit.craftbukkit.Main -jar spigot-1.13.2.jar
+java -Xms512M -Xmx1G -XX:+UseConcMarkSweepGC -classpath sc-mqtt.jar;spigot-1.13.2.jar org.bukkit.craftbukkit.Main
 pause
